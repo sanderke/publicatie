237138dbffaa61e648906ba4525c9af3a4277ee0
GitHub Action <api@logius.nl>
2022-11-28 16:39:00 +0000

    Release: ep-ohnl-factuur-akkoord

--- ep/ohnl-factuur-akkoord/js/organisation-config.js
+++ ep/ohnl-factuur-akkoord/js/organisation-config.js
@@ -4,7 +4,7 @@ var organisationConfig = {
 
     // this url points to the folder where organsation specific css files are stored
     // defaults to https://tools.geostandaarden.nl/respec/style/ if not set
-    nl_organisationStylesURL: "https://publicatie.centrumvoorstandaarden.nl/respec/style/",
+    nl_organisationStylesURL: "https://gitdocumentatie.logius.nl/publicatie/respec/style/",
 
     // prefix for the names of company specific css, svg and ico prefixes
     // defaults to "GN-"
@@ -12,25 +12,33 @@ var organisationConfig = {
 
     // nl_organisationPublishURL points to organisation specifica publication page, used in header
     // defaults to  https://docs.geostandaarden.nl/"
-    nl_organisationPublishURL: "https://publicatie.centrumvoorstandaarden.nl/",
+    nl_organisationPublishURL: "https://gitdocumentatie.logius.nl/publicatie/",
 
     // nl_logo refers to company logo
-    // defaults to https://tools.geostandaarden.nl/respec/style/logos/Geonovum.svg
     nl_logo: {
-        src: "https://publicatie.centrumvoorstandaarden.nl/respec/style/logos/figure-logius.svg",
+        src: "https://gitdocumentatie.logius.nl/publicatie/respec/style/logos/figure-logius.svg",
         alt: "Logius",
         id: "Logius",
         height: 77,
         width: 44,
         url: "https://www.logius.nl/standaarden",
     },
-	
-	localBiblio: {
-	"SemVer": {
-            href: "https://semver.org",
-            title: "Semantic Versioning 2.0.0",
-            authors: ["T. Preston-Werner"],
-            date: "June 2013"
-        }
+    
+    localBiblio: {
+        "Logius website": {
+            href: "https://www.logius.nl/diensten/e-procurement",
+            title: "Logius e-procurement",
+            publisher: "Logius",
+        },
+        "UBL-OHNL": {
+            href: "https://logius.nl/diensten/digiinkoop/hoe-werkt-het/ubl-ohnl",
+            titel: "UBL-OHNL",
+            publisher: "Logius",
+        },
+        "SETU (HR-XML) - OHNL": {
+            href: "https://logius.nl/diensten/digiinkoop/hoe-werkt-het/setu-hr-xml-ohnl",
+            titel: "SETU (HR-XML) - OHNL",
+            publisher: "Logius",
+        },
     },
 }
