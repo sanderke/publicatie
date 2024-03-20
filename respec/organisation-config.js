996f0272606ebd0f38b5fc341f20c3ca14e97282
Hugo Stijns <hugo@stijns.net>
2024-03-20 11:48:39 +0100

    Fix invalid organisation config

--- respec/organisation-config.js
+++ respec/organisation-config.js
@@ -474,16 +474,16 @@ var organisationConfig = {
             publisher: "Geonovum",
             date: "November 2023"
         },
-        "JRC117836" { 
+        "JRC117836": {
             href: "https://op.europa.eu/en/publication-detail/-/publication/6521f427-01df-11ea-8c1f-01aa75ed71a1",
-            title: "The Relationship Between Open Source Software and Standard Setting",
+            title: "The Relationship Between Open Source Software and Standard Setting",
             authors: ["Knut Blind", "Mirko Böhm"],
             editors: ["Nikolaus Thumm"],
             publisher: "Joint Research Center",
             id: "JRC117836",
             date: "2019"
         },
-        "Krechmer2002" {
+        "Krechmer2002": {
             href: "https://www.isology.com/pdf/cathedrals.pdf",
             title: "Cathedrals, Libraries and Bazaars",
             authors: ["Ken Krechmer"],
